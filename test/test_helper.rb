69fb919fe44e21d91a9b06b2cc1a87afddb3eb66
Lucas Sousa <30642647+lsouoliveira@users.noreply.github.com>
2025-07-12 11:40:11 -0300

    Add base test class

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,9 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "ruby_ui_mcp"
 
 require "minitest/autorun"
+
+class BaseTest < Minitest::Test
+  def fixture_file(name)
+    File.read(File.expand_path(File.join("fixtures", name), __dir__))
+  end
+end
